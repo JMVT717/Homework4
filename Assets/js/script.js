b43e5197ab53409e6a302ad50ba15b4d1e1dfdb5
jmvt717 <jmvt717@josephmilton.world>
2021-08-10 14:06:47 -0500

    start button works

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,27 @@
 let startBttn = document.querySelector("#start-quiz");
+var timeEl = document.querySelector(".timer-sec");
 
+var secondsLeft = 180;
 
 function start (){ //start button
-    startContainer.setAttribute("class", "hidden");
+    // startContainer.setAttribute("class", "hidden");
+    setTime()
 
 }
-startBttn.addEventListener("click", start)
+startBttn.addEventListener("click", start);
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
