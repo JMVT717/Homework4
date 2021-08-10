71aa6a14dcec3ae2bc322c62134c1087e51bc678
jmvt717 <jmvt717@josephmilton.world>
2021-08-10 13:55:07 -0500

    added start button and function

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+let startBttn = document.querySelector("#start-quiz");
+
+
+function start (){ //start button
+    startContainer.setAttribute("class", "hidden");
+
+}
+startBttn.addEventListener("click", start)
